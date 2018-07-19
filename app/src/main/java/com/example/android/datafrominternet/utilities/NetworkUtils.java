af69b3681ac54bd595dbdcfb9ed3bf2471f44e1f
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-07-19 22:59:27 +0530

    Utilities will be used to communicate with network consuming base URL

--- app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -1,4 +1,13 @@
 package com.example.android.datafrominternet.utilities;
 
+/**
+ * These utilities will be used to communicate with the network.
+ */
 public class NetworkUtils {
+
+    final static String GITHUB_BASE_URL =
+            "https://api.github.com/search/repositories";
+
+    final static String PARAM_QUERY = "q";
+
 }
