24f36620fddddd54927f4190c7b96013a9b101fd
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-08-06 16:01:23 +0530

    Create a variable to store a reference to the error message TextView

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
+    private TextView mErrorMessageDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
